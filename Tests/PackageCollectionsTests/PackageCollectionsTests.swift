158a19b90e3a8daa051c1e9378a74356b409a54b
Sven A. Schmidt <sas@finestructure.co>
2025-04-17 10:23:06 +0200

    Add a basic test

--- Tests/PackageCollectionsTests/PackageCollectionsTests.swift
+++ Tests/PackageCollectionsTests/PackageCollectionsTests.swift
@@ -2,6 +2,6 @@ import Testing
 @testable import PackageCollectionsModel
 @testable import PackageModel
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func Target_init() async throws {
+    #expect(PackageCollectionModel.V1.Target(name: "foo", targetName: "foo-bar") == .init(name: "foo", moduleName: "foo_bar"))
 }
